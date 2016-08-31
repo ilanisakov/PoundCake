78d1ab6c211aa768458f692d39a4ba0ab6a9f0fc
Ilan Isakov <ixi7500@rit.edu>
2016-08-31 09:41:35 -0400

    Cleaned up ProjectileClass

--- Assets/Scripts/ProjectileClass.cs
+++ Assets/Scripts/ProjectileClass.cs
@@ -5,20 +5,8 @@ public class ProjectileClass : MonoBehaviour {
 
     public float lifetime = 2.0f;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
     void Awake()
     {
         Destroy(gameObject, lifetime);
     }
-
-
 }
